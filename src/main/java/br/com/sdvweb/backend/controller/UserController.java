5a7d09ce191000f06e2862e79bfcf8aa7f01737b
unknown <leandrofreitas.dev@gmail.com>
2025-06-21 20:14:48 -0300

    corsapi

--- src/main/java/br/com/sdvweb/backend/controller/UserController.java
+++ src/main/java/br/com/sdvweb/backend/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import br.com.sdvweb.backend.dto.UserDTO;
 import br.com.sdvweb.backend.service.UserService;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(value = "/usuario")
 public class UserController {
