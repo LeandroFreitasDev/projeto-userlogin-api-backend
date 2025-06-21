99a3ce4685c9362a6c7afa8c487a4bc8820bdcab
unknown <leandrofreitas.dev@gmail.com>
2025-06-21 13:09:06 -0300

    baseproject

---
+++ src/main/java/br/com/sdvweb/backend/service/UserService.java
@@ -0,0 +1,38 @@
+package br.com.sdvweb.backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.sdvweb.backend.dto.UserDTO;
+import br.com.sdvweb.backend.entity.User;
+import br.com.sdvweb.backend.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	public UserRepository userRepository;
+
+	public List<UserDTO> listarTodos() {
+		List<User> user = userRepository.findAll();
+		return user.stream().map(UserDTO::new).toList();
+	}
+
+	public void inserir(UserDTO usuario) {
+		User user = new User(usuario);
+		userRepository.save(user);
+	}
+
+	public UserDTO alterar(UserDTO usuario) {
+		User user = new User(usuario);
+		return new UserDTO(userRepository.save(user));
+	}
+
+	public void excluir(Long id) {
+		User user = userRepository.findById(id).get();
+		userRepository.delete(user);
+	}
+
+}
