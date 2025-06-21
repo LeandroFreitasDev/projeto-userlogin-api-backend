18388bd4274cf0942c73f74eae0da6c750adf978
unknown <leandrofreitas.dev@gmail.com>
2025-06-21 10:53:42 -0300

    conexaodb

---
+++ src/main/java/br/com/sdvweb/backend/UserloginApplication.java
@@ -0,0 +1,13 @@
+package br.com.sdvweb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserloginApplication.class, args);
+	}
+
+}
